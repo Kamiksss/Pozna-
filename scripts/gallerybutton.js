28a6b6598667721cd4234ad04f1ac6b020db4156
Kamiksss <blue.kamlotov@gmail.com>
2023-04-02 15:17:01 +0200

    galeria

--- scripts/gallerybutton.js
+++ scripts/gallerybutton.js
@@ -3,20 +3,20 @@ const imgQuantity = 3;
 
 galleryNext = () => {
   myImage = document.getElementById("galleryIMG");
-  myImage.src = `./img/index/${displayed + 1}.png`;
+  myImage.src = `../img/index/${displayed + 1}.png`;
   displayed += 1;
   if (displayed > imgQuantity) {
-    myImage.src = "./img/index/1.png";
+    myImage.src = "../img/index/1.png";
     displayed = 1;
   }
 };
 
 galleryPrev = () => {
   myImage = document.getElementById("galleryIMG");
-  myImage.src = `./img/index/${displayed - 1}.png`;
+  myImage.src = `../img/index/${displayed - 1}.png`;
   displayed -= 1;
   if (displayed < 1) {
-    myImage.src = `./img/index/${imgQuantity}.png`;
+    myImage.src = `../img/index/${imgQuantity}.png`;
     displayed = imgQuantity;
   }
 };
