0c25d605914253d06953c78e97b7d68072d97d7c
Kamiksss <blue.kamlotov@gmail.com>
2023-04-02 14:51:30 +0200

    wreszcie coś jest zrobione

---
+++ scripts/email.js
@@ -0,0 +1,8 @@
+function emailSubmit() {
+  let your_email = document.getElementById("emial").value;
+
+  window.alert(
+    "Twoja wiadomość została wysłana, oczekuj informacji zwrotniej pod adresem: " +
+      your_email
+  );
+}
---
+++ scripts/gallerybutton.js
@@ -0,0 +1,22 @@
+var displayed = 1;
+const imgQuantity = 3;
+
+galleryNext = () => {
+  myImage = document.getElementById("galleryIMG");
+  myImage.src = `./img/index/${displayed + 1}.png`;
+  displayed += 1;
+  if (displayed > imgQuantity) {
+    myImage.src = "./img/index/1.png";
+    displayed = 1;
+  }
+};
+
+galleryPrev = () => {
+  myImage = document.getElementById("galleryIMG");
+  myImage.src = `./img/index/${displayed - 1}.png`;
+  displayed -= 1;
+  if (displayed < 1) {
+    myImage.src = `./img/index/${imgQuantity}.png`;
+    displayed = imgQuantity;
+  }
+};
