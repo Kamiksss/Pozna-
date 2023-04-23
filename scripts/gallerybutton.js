0f4c33181601503f4fbb63186721382b68001c3a
Kamiksss <blue.kamlotov@gmail.com>
2023-04-23 19:39:36 +0200

    finał

--- scripts/gallerybutton.js
+++ scripts/gallerybutton.js
@@ -3,10 +3,10 @@ const imgQuantity = 38;
 
 galleryNext = () => {
   myImage = document.getElementById("galleryIMG");
-  myImage.src = `../img/gallery/gallery (${displayed + 1}).jpg`;
+  myImage.src = `../img/gallery/gallery(${displayed + 1}).jpg`;
   displayed += 1;
   if (displayed > imgQuantity) {
-    myImage.src = "../img/gallery/gallery (1).JPG";
+    myImage.src = "../img/gallery/gallery(1).jpg";
     displayed = 1;
   }
   console.log(displayed);
@@ -14,10 +14,10 @@ galleryNext = () => {
 
 galleryPrev = () => {
   myImage = document.getElementById("galleryIMG");
-  myImage.src = `../img/gallery/gallery (${displayed - 1}).jpg`;
+  myImage.src = `../img/gallery/gallery(${displayed - 1}).jpg`;
   displayed -= 1;
   if (displayed < 1) {
-    myImage.src = `../img/gallery/gallery (${imgQuantity}).jpg`;
+    myImage.src = `../img/gallery/gallery(${imgQuantity}).jpg`;
     displayed = imgQuantity;
   }
 };
